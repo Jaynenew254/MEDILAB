ec23c6de6e52f9c8b75e5b8b071dbf3486645c5b
jaynenew254 <jayne8107@gmail.com>
2024-05-29 09:12:42 +0300

    added labtest

--- app.py
+++ app.py
@@ -3,8 +3,19 @@ from flask_restful import Api
 app=Flask (__name__)
 api = Api(app)
 
+from datetime import timedelta
+from flask_jwt_extended import JWTManager
+# set up jwt 
+
+app.secret_key ='(!@#$%^&*)'
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=30)
+jwt = JWTManager(app)
+
+
+
 # end points 
-from views . views import MemberSignup ,MemberSignin,MemberProfile,AddDependant,ViewDependant
+from views . views import MemberSignup ,MemberSignin,MemberProfile,AddDependant,ViewDependant,ViewAllLabs,LabTest,MakeBooking,MyBooking
 api.add_resource(MemberSignup,'/api/member_signup')
 
 api.add_resource(MemberSignin,'/api/member_signin')
@@ -14,6 +25,14 @@ api.add_resource(MemberProfile,'/api/member_profile')
 api.add_resource(AddDependant,'/api/add_dependant')
 
 api.add_resource(ViewDependant,'/api/view_dependant')
+
+api.add_resource(ViewAllLabs,'/api/laboratories')
+
+api.add_resource(LabTest,'/api/lab_test')
+
+api.add_resource(MakeBooking,'/api/make_booking')
+
+api.add_resource(MyBooking,'/api/mybooking')
 
 if __name__ == '__main__' :
     app.run(debug=True)
--- views/views.py
+++ views/views.py
@@ -45,6 +45,9 @@ from flask import *
 import pymysql.cursors
 from functions import *
 
+# IMPORT JWT packageS
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required 
+
 # Add a member class
 # member_signup and member_signin
 class MemberSignup(Resource):
@@ -107,7 +110,10 @@ class MemberSignin(Resource):
             hashed_password = member['password']
             is_matchpassword =hash_verify(password,hashed_password)
             if is_matchpassword == True:
-                return jsonify({"message": "login successful"})
+                access_token = create_access_token(identity=member,fresh=True)
+                return jsonify({"access_token":access_token,
+                                "member":member
+                                })
             elif is_matchpassword == False:
                 return jsonify({"message": "Login Failed"})
             else:
@@ -119,6 +125,7 @@ class MemberSignin(Resource):
      # memberprofile
 
 class MemberProfile(Resource):
+    @jwt_required(fresh=True)
     def post(self):
         data =request.json
         members_id =data ["members_id"]
@@ -139,6 +146,7 @@ class MemberProfile(Resource):
         
 # add a dependant
 class AddDependant(Resource):
+    @jwt_required(fresh=True)
     def post(self):
         data= request.json
         members_id =data ["members_id"]
@@ -163,6 +171,7 @@ class AddDependant(Resource):
         
 # view dependant based on member id 
 class ViewDependant(Resource):
+    @jwt_required(fresh=True)
     def post(self):
         data = request.json
         members_id = data ["members_id"]
@@ -177,6 +186,122 @@ class ViewDependant(Resource):
         else:
             dependants = cursor.fetchall()
             return jsonify({"message": dependants})
+        
+    # see all labs
+class ViewAllLabs(Resource):
+    def get(self):
+        connection = pymysql.connect(host='localhost', user='root', password='', database='Medilab')
+        sql = "SELECT * FROM laboratories "
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+
+        if cursor.rowcount == 0:
+            return jsonify({"message":"No Laboratories"})
+        else:
+            labs =cursor.fetchall()
+            return jsonify(labs)
+# create a lab test resourse 
+class LabTest(Resource):
+    def post(self):
+        data = request.json
+        lab_id = data ["lab_id"]
+
+        # create a connection 
+        connection = pymysql.connect(host='localhost', user='root', password='', database='Medilab')
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        sql = "SELECT * FROM lab_tests where lab_id =%s"
+        cursor.execute(sql, lab_id)
+        if cursor.rowcount == 0:
+            return jsonify({"message":"No Lab Test Found"})
+        else:
+            lab_test =cursor.fetchall()
+            return jsonify(lab_test)
+        
+
+        
+# 1. create a Resource called  make bookigs
+# the Resource shouled allow post ethode
+# save the booking in data BaseException
+class MakeBooking(Resource):
+    def post(self):
+        data = request.json
+        member_id =data["member_id"]	
+        booked_for = data["booked_for"]	
+        dependant_id= data["dependant_id"]	
+        test_id=data=["test_id"]	
+        appointment_date=["appointment_date"]	
+        appointment_time=["appointment_time"]
+        where_taken	=["where taken"]
+        latitude=["latitude"]	
+        longitude=["longitude"]	
+        status=["status"]	
+        lab_id	=["lab_id"]
+
+         # connect to DB
+        connection = pymysql.connect(host='localhost', user='root', password='', database='Medilab')
+        cursor = connection.cursor()
+
+        
+        sql = "INSERT INTO BOOKINGS(member_id,booked_for, dependant_id,	test_id	,appointment_date,appointment_time,where_taken,latitude,longitude,status,lab_id	) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)" 	
+        data=(member_id	,booked_for	,dependant_id,test_id,appointment_date,appointment_time,where_taken	,latitude,longitude	,status	,lab_id		
+)
+        cursor.execute(sql, data)
+        member = cursor.fetchone()
+
+        if member:
+            # member exists, check if class is available
+            cursor.execute(sql, data)
+            class_ = cursor.fetchone()
+
+        if class_:
+                # class is available, make booking
+            sql= "INSERT INTO BOOKINGS(member_id,booked_for, dependant_id,	test_id	,appointment_date,appointment_time,where_taken,latitude,longitude,status,lab_id	) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)" 	
+            data=(member_id	,booked_for	,dependant_id,test_id,appointment_date,appointment_time,where_taken	,latitude,longitude	,status	,lab_id	)
+            cursor.execute(sql, data)
+            connection.commit()
+
+            #     # update class availability
+            # sql = "UPDATE classes SET available = 0 WHERE class_name = %s AND class_date = %s"
+            # data = (booked_class, booked_date)
+            # cursor.execute(sql, data)
+            # connection.commit()
+
+                # return success message
+            return jsonify ({"message": "Booking made successfully"})
+        
+        #  elif
+        #         # class is not available, return error message
+        #         return jsonify({"message": "Class is not available"})
+        
+
+        else:
+            # member does not exist, return error message
+             return jsonify({"message": "Member not found"})
+
+        
+
+
+# 2. create a Resource called my booking 
+# the Resource should allow get method
+# get booking using member _id
+class MyBooking(Resource):
+    def get(self):
+        data = request.json
+        members_id = data["members_id"]
+        # connect to DB
+        connection = pymysql.connect(host='localhost', user='root', password='', database='Medilab')
+        cursor = connection.cursor()
+        sql = "SELECT * FROM booking  WHERE member_id = %s"
+        cursor.execute(sql, members_id)
+        count = cursor.rowcount
+        if count == 0:
+            return jsonify({"message": "member does not exist"})
+        else:
+            MyBooking = cursor.fetchall()
+            return jsonify({"message": MyBooking})
+
+        
+
         
         
 
